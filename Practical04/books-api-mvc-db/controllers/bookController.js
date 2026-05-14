8986cf4803a4b60522eadc2b7dd3c841175221bd
Ethan-hub-dot <s10273704@connect.np.edu.sg>
2026-05-14 11:04:46 +0800

    Commit practical 4

---
+++ Practical04/books-api-mvc-db/controllers/bookController.js
@@ -0,0 +1,69 @@
+const bookModel = require("../models/bookModel");
+
+// Get all books
+async function getAllBooks(req, res) {
+  try {
+    const books = await bookModel.getAllBooks();
+    res.json(books);
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error retrieving books" });
+  }
+}
+
+// Get book by ID
+async function getBookById(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    const book = await bookModel.getBookById(id);
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.json(book);
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error retrieving book" });
+  }
+}
+
+// Create new book
+async function createBook(req, res) {
+  try {
+    const newBook = await bookModel.createBook(req.body);
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error creating book" });
+  }
+}
+
+async function updateBook(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    const updatedBook = await bookModel.updateBook(id, req.body);
+    res.json(updatedBook);
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error updating book" });
+  }
+}
+
+async function deleteBook(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    await bookModel.deleteBook(id);
+    res.json({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error deleting book" });
+  }
+}
+
+module.exports = {
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook,
+};
